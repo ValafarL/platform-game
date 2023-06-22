6973516fc841e64fd8a84c3348a5ee18c5593d9c
LucasBourscheidC <lucasbc12022001@gmail.com>
2023-06-21 22:19:44 -0300

    Revert "."
    This reverts commit dd31196746b4405631cb199ccfec26515f49e946.

--- Jogo/stdfx.cpp
+++
@@ -1,1 +0,0 @@
-#include "stdfx.h"
--- Jogo/stdfx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-using namespace std;
-#include <SFML/Graphics.hpp>
-#include <string.h>
-#include <vector>
-using namespace std;
-
-
